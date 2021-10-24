cc8530ac10f0b4997c031a14bd9fd7164fe1df8b
nakajima85 <91404902+nakajima85@users.noreply.github.com>
2021-10-24 20:13:13 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get 'messages/index'
   root to: "messages#index"
   # ルートパスへのアクセスがあったら、messages_controllerのindexアクションが呼び出される
+  resources :users, only: [:edit, :update]
 end
