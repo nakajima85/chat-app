7555ece8d466274c83a9ef06371f2d49b8849b04
nakajima85 <91404902+nakajima85@users.noreply.github.com>
2021-10-27 09:23:21 +0900

    新規制作画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   root to: "messages#index"
   # ルートパスへのアクセスがあったら、messages_controllerのindexアクションが呼び出される
   resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create]
 end
